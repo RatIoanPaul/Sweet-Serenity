4f9c5aa0d951223f885ac5145402670b7752ff9a
rancovlarisa24 <165498284+rancovlarisa24@users.noreply.github.com>
2024-11-05 18:46:22 +0200

    Revert "Vizualizare produse + Vizualizare cos"

--- sweet-design/sweetDesign/src/pages/homePage/index.jsx
+++ sweet-design/sweetDesign/src/pages/homePage/index.jsx
@@ -131,6 +131,11 @@ const Home = () => {
                         <p>Visit us or follow us on social media for the latest updates!</p>
                     </div>
                 </footer>
+                {(userRole === "CUSTOMER") && (
+                    <div id="options-section">
+                        <p>Salut!</p>
+                    </div>
+                )}
             </>
 
             );
--- sweet/src/main/java/tw_Project/sweet/config/CorsConfig.java
+++ sweet/src/main/java/tw_Project/sweet/config/CorsConfig.java
@@ -1,22 +1,4 @@
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+package tw_Project.sweet.config;
 
-@Configuration
-public class GlobalCorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:5173"); // Permite accesul de pe frontend
-        config.addAllowedHeader("*"); // Permite toate antetele
-        config.addAllowedMethod("*"); // Permite toate metodele HTTP (GET, POST, PUT, DELETE etc.)
-        config.setAllowCredentials(true); // Permite trimiterea de credențiale (cookie-uri)
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config); // Se aplică pentru toate endpoint-urile
-        return new CorsFilter(source);
-    }
+public class CorsConfig {
 }
